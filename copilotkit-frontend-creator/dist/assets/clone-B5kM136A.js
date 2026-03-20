f67b3018dd1ef72adebf734c92b11453e840c4ae
Your Name <your-email@example.com>
2026-03-20 20:11:25 +0530

    rebuild dist with updated generator deps

--- copilotkit-frontend-creator/dist/assets/clone-B5kM136A.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./_baseUniq-D3woHUlM.js";var e=4;function a(o){return r(o,e)}export{a as c};
---
+++ copilotkit-frontend-creator/dist/assets/clone-BDxfCgde.js
@@ -0,0 +1,1 @@
+import{b as r}from"./_baseUniq-ahaV6Aur.js";var e=4;function a(o){return r(o,e)}export{a as c};
